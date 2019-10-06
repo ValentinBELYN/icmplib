2694aed3aa9150253c869d58e6b46bf4d6adbfec
ValentinBELYN <35041719+ValentinBELYN@users.noreply.github.com>
2019-10-06 16:16:38 +0200

    Add some examples

---
+++ examples/multiping_example.py
@@ -0,0 +1,46 @@
+'''
+    multiping example - icmplib
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        https://github.com/ValentinBELYN/icmplib
+
+    :copyright: Copyright 2017-2019 Valentin BELYN.
+    :license: GNU GPLv3, see LICENSE for details.
+'''
+
+from icmplib import multiping
+
+
+addresses = [
+    # A fully qualified domain name (FQDN) is allowed. The first
+    # address returned from the DNS resolution will be used.
+    # For deterministic behavior, prefer to use an IP address.
+    'github.com',
+
+    # IPv4 addresses
+    '1.1.1.1',
+    '8.8.8.8',
+    '10.0.0.100',
+    '10.0.0.200',
+
+    # IPv6 addresses
+    '::1',
+]
+
+hosts = multiping(addresses, count=2, interval=0.5, timeout=1)
+
+hosts_alive = []
+hosts_dead = []
+
+for host in hosts:
+    if host.is_alive:
+        hosts_alive.append(host.address)
+
+    else:
+        hosts_dead.append(host.address)
+
+print(hosts_alive)
+# ['github.com', '1.1.1.1', '8.8.8.8', '::1']
+
+print(hosts_dead)
+# ['10.0.0.100', '10.0.0.200']
---
+++ examples/ping_example.py
@@ -0,0 +1,48 @@
+'''
+    ping example - icmplib
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+        https://github.com/ValentinBELYN/icmplib
+
+    :copyright: Copyright 2017-2019 Valentin BELYN.
+    :license: GNU GPLv3, see LICENSE for details.
+'''
+
+from icmplib import ping
+
+
+host = ping('1.1.1.1', count=10, interval=0.5, timeout=1)
+
+# The IP address of the gateway or host that responded to the request
+print(host.address)
+# '1.1.1.1'
+
+# The minimum round-trip time
+print(host.min_rtt)
+# 12.2
+
+# The average round-trip time
+print(host.avg_rtt)
+# 13.2
+
+# The maximum round-trip time
+print(host.max_rtt)
+# 17.6
+
+# The number of packets transmitted to the destination host
+print(host.transmitted_packets)
+# 10
+
+# The number of packets sent by the remote host and received by the
+# current host
+print(host.received_packets)
+# 9
+
+# Packet loss occurs when packets fail to reach their destination
+# Return a float between 0 and 1 (all packets are lost)
+print(host.packet_loss)
+# 0.1
+
+# Return True if the host is reachable, False otherwise
+print(host.is_alive)
+# True
